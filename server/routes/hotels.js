bf9ab1e3df6027b4319cf2f04939d89f354e9750
thepriyansh01 <priyansh.m.2002@gmail.com>
2023-05-12 08:57:47 +0530

    solve errors in hotelRoutes

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,62 +1,74 @@
-import Express from "express";
-import Hotel from "../models/hotel.js";
-const router = Express.Router();
+import express from 'express';
+import mongoose from 'mongoose';
+import Hotel from '../models/hotel.js';
 
+const router = express.Router();
 
-// CREATE 
-router.post('/create', async (req, res) => {
+// CREATE operation
+router.post('/', async (req, res) => {
+  try {
     const newHotel = new Hotel(req.body);
+    await newHotel.save();
+    res.status(201).json(newHotel);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (error) {
-        console.log(error);
-    }
-})
-// UPDATE
-router.put('/update/:id', async (req, res) => {
+// READ operation to fetch all hotels
+router.get('/', async (req, res) => {
+  try {
+    const hotels = await Hotel.find({});
+    res.json(hotels);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-        res.status(200).json(updatedHotel);
-    } catch (error) {
-        console.log(error);
-    }
-})
-
-// DELETE
-router.delete('/delete/:id', async (req, res) => {
-
-    try {
-        await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("Hotel has been deleted.");
-    } catch (error) {
-        console.log(error);
-    }
-})
-
-// GET
+// READ operation to fetch a single hotel by ID
 router.get('/:id', async (req, res) => {
-
-    try {
-        const foundhotel = await Hotel.findById(req.params.id);
-        res.status(200).json(foundhotel);
-    } catch (error) {
-        console.log(error);
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    if (!hotel) {
+      return res.status(404).json({ message: 'Hotel not found' });
     }
-})
+    res.json(hotel);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
-// GETALL
-router.get('/hotels', async (req, res) => {
-
-    try {
-        const hotels = await Hotel.find({});
-        res.status(200).json(hotels);
-    } catch (error) {
-        console.log(error);
+// UPDATE operation
+router.patch('/:id', async (req, res) => {
+  try {
+    const hotel = await Hotel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!hotel) {
+      return res.status(404).json({ message: 'Hotel not found' });
     }
-})
+    res.json(hotel);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
+// DELETE operation
+router.delete('/:id', async (req, res) => {
+  try {
+    const hotel = await Hotel.findByIdAndDelete(req.params.id);
+    if (!hotel) {
+      return res.status(404).json({ message: 'Hotel not found' });
+    }
+    res.json({ message: 'Hotel deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
 export default router;
